baf8a1640262e3fd5d593d484507d589e8d7549a
RazanGurung <razangurung2147@gmail.com>
2021-08-01 17:42:55 +0545

    job appl

--- routes/applyjob.js
+++ routes/applyjob.js
@@ -2,21 +2,22 @@ const express = require('express');
 const ApplyJob = require('../models/applyjob.js');
 const router = express.Router();
 
-router.post("/apply/job", function(req,res){
+router.post("/apply/job",function(req,res){
     const userid = req.body.userid;
     const workid = req.body.workid;
     const application = req.body.application;
-
     const applyjob = new ApplyJob({
         userid:userid,
         workid:workid,
-        application:application
+        application:application,
+    });
+    applyjob.save()
+    .then(function(result){
+        res.status(201).json({message : "Applied Job Successfully",success:true})
     })
-    applyjob.save().then(data=>{
-        res.status(200).json({message:"job Application Successful"})
-    }).catch(err=>{
-        res.status(500).json(err)
-    })
-})
+    .catch(function(err){
+        res.status(500).json({message : err,success:false})
+    });
+});
 
 module.exports = router
